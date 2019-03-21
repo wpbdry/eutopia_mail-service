34169472b43fc90fe98c56d78594fabe05496db8
William Dry <wpbdry@gmail.com>
2019-03-21 13:14:26 +0100

    require password to send email

--- sendmail_schema.py
+++ sendmail_schema.py
@@ -1,6 +1,7 @@
 import graphene
 
 import gmail
+import secret
 
 class Query(graphene.ObjectType):
     hello = graphene.String(argument=graphene.String(default_value="stranger"))
@@ -14,13 +15,17 @@ class SendMail(graphene.Mutation):
         recipient = graphene.String()
         subject = graphene.String()
         body = graphene.String()
+        password = graphene.String()
 
     ok = graphene.Boolean()
     msg = graphene.String()
 
-    def mutate(self, info, recipient, subject, body):
-        result = gmail.send_mail(recipient, subject, body)
-        return SendMail(ok=result["ok"], msg=result["msg"])
+    def mutate(self, info, recipient, subject, body, password):
+        if password == secret.mail_service_password:  # Check password
+            result = gmail.send_mail(recipient, subject, body)
+            return SendMail(ok=result["ok"], msg=result["msg"])
+        else:
+            return SendMail(ok=False, msg="Authentication error")
 
 
 class Mutations(graphene.ObjectType):
